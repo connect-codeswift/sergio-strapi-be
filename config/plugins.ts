80099f019a0c6ab0f9a92949f48d9bba743dfca1
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2026-03-07 05:15:38 +0500

    feat: plugin added

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,29 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  // add smtp strapi configuration for google SMTP server
+  // TLS 587
+  // SLS 465
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env.int("SMTP_PORT", 587),
+        secure: env.bool("SMTP_SECURE", false),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: env("SMTP_USERNAME"),
+        defaultReplyTo: env("SMTP_USERNAME"),
+      },
+    },
+  },
+});
 
 export default config;
