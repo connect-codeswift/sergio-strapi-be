cd2e45a81b372f39b5529397a061f43ef5bed871
CodeSwift <connect@codeswift.org>
2026-03-10 21:40:47 +0500

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -11,6 +11,29 @@ const config: Core.Config.Middlewares = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  import type { Core } from '@strapi/strapi';
+
+const config: Core.Config.Middlewares = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::public',
+    config: {
+      maxAge: 60000, // 60,000ms = 1 minute
+    },
+  },
+];
+
+export default config;
+
 ];
 
 export default config;
